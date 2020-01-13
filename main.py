03993f4fa9d784a8b4c91321c932900c61913fdf
Aleksey Semenov <7.on.off@bk.ru>
2020-01-13 18:47:00 +0300

    Реализует обработку ошибок в функции list_all_owner

--- main.py
+++ main.py
@@ -293,17 +293,27 @@ def doc_program(doc, dirs):
     def list_all_owner(arg):
         docs = arg['documents']
         command = arg['command']
+        names = set()
+        errors = 0
         counter = 0
         if len(docs) > 0:
             for doc in docs:
-                counter += 1
+
                 try:
                     name = doc['name']
                 except KeyError as e:
-                    command['message'] = f'Ошибка: Поле {e} в документе отсутствует'
-                    return False
+                    errors += 1
+                    break
                 else:
+                    names.add(name)
+            if len(names) > 0:
+                for name in names:
+                    counter += 1
                     command['message'] += f'{counter}. {name}\n'
+            else:
+                command['message'] = 'Имена не найдены'
+            if errors > 0:
+                command['message'] += f'---------\nДокументов с несуществующим ключем "name": {errors}'
         else:
             command['message'] = 'Документов нет'
             return False
