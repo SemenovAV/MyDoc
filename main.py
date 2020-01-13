97a1c81547fdab26b9c536c4e971cf1b02b942f3
Aleksey Semenov <7.on.off@bk.ru>
2020-01-13 18:53:22 +0300

    Рефакторинг.
    Выносит из основного файла переменные с данными.

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-documents = [
-    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-    {"type":"0"}
-]
-directories = {
-    '1': ['2207 876234', '11-2', '5455 028765'],
-    '2': ['10006', '5400 028765', '5455 002299'],
-    '3': []
-}
+from data.documents import documents
+from data.directories import directories
 
 
 def doc_program(doc, dirs):
